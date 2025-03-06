44f98a7c702dd80b15721090db8836129ff27b38
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2025-03-06 18:33:50 +0530

    learning done of prisma and connecting with online database

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,60 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-async function insertUserData(username :string, password:string , firstname:string , lastname:string
- ){ 
-   const user = await prisma.user.create({
-        data : {
-         username : username,
-         password : password, 
-         firstname :firstname,
-         lastname:lastname
-        }
-    });
-    console.log(user);
+async function insertUserData(
+  username: string,
+  password: string,
+  firstname: string,
+  lastname: string
+) {
+  const user = await prisma.user.create({
+    data: {
+      username: username,
+      password: password,
+      firstname: firstname,
+      lastname: lastname,
+    },
+  });
+  console.log(user);
 }
-async function findUserUsingUserName(username : string) {
-    const res = await prisma.user.findUnique({
-        where : {
-            username : username
-        }
-    })
-    console.log(res)
+async function findUserUsingUserName(username: string) {
+  const res = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+  console.log(res);
 }
-async function updateingFirstName(username : string , updateFirstName : string) {
-    const res = await prisma.user.update({
-        where : {
-            username : username
-        },
-        data:{
-            firstname : updateFirstName
-        }
-    })
-    console.log(res);
+async function updateingFirstName(username: string, updateFirstName: string) {
+  const res = await prisma.user.update({
+    where: {
+      username: username,
+    },
+    data: {
+      firstname: updateFirstName,
+    },
+  });
+  console.log(res);
 }
-async function inserTodos(title : string , description : string , user_id : number) { 
-    const res = await prisma.todo.create({
-        data : {
-            title : title,
-            description : description,
-            user_id : user_id
-        }
-    })
-    console.log(res) ;
+async function inserTodos(title: string, description: string, user_id: number) {
+  const res = await prisma.todo.create({
+    data: {
+      title: title,
+      description: description,
+      user_id: user_id,
+    },
+  });
+  console.log(res);
 }
-async function findTodosUsingUserid(userid :number) {
-    const res = await prisma.todo.findMany({
-        where:{
-            user_id : userid
-        }
-    })
-    console.log(res);
+async function findTodosUsingUserid(userid: number) {
+  const res = await prisma.todo.findMany({
+    where: {
+      user_id: userid,
+    },
+  });
+  console.log(res);
 }
 // insertUserData("kisan02" , "123" , "kisan" , "sharma");
 // findUserUsingUserName("bharat00")
 // updateingFirstName("bharat007" , "anusri")
 // inserTodos("going to school " , "need to learning more coding" , 2)
-findTodosUsingUserid(2)
+findTodosUsingUserid(2);
